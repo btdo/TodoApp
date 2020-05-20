43e3de5372290442bb7da2ab1b5b99a683243713
bao do <baodo210@hotmail.com>
2020-05-19 22:48:31 -0400

    Added: Query and list task List

--- app/src/main/java/com/wwm/todo/HomeFragment.kt
+++ app/src/main/java/com/wwm/todo/HomeFragment.kt
@@ -16,6 +16,7 @@ import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient
 import com.amazonaws.mobileconnectors.appsync.fetcher.AppSyncResponseFetchers
 import com.apollographql.apollo.GraphQLCall
 import com.apollographql.apollo.exception.ApolloException
+import com.wwm.todo.auth.AuthenticationServiceImpl
 import com.wwm.todo.databinding.FragmentHomeBinding
 import javax.annotation.Nonnull
 
@@ -33,7 +34,6 @@ class HomeFragment : Fragment() {
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        val application = requireActivity().application
         val viewModelFactory = HomeFragmentViewModelFactory()
         binding = FragmentHomeBinding.inflate(inflater)
         binding.lifecycleOwner = this
@@ -46,11 +46,12 @@ class HomeFragment : Fragment() {
 
         mAWSAppSyncClient = AWSAppSyncClient.builder()
             .context(requireContext())
-            .oidcAuthProvider { "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IllvQXA3WmR5YktDT3NJa2JxYnlwZyJ9.eyJpc3MiOiJodHRwczovL2Rldi1iaWRmbG9xOC5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWVjMGFiYmQ0MGZhNTYwYzc1NjMxMTQxIiwiYXVkIjoidko2emJpMFNkc2lKNTJzZVd3RGYwVDBONnhqZllmbTYiLCJpYXQiOjE1ODk5MzYwNzQsImV4cCI6MTU4OTk3MjA3NCwibm9uY2UiOiJmVnlncEduWXU5TUxpX0ViaXlkVmtVWFlaNGYybVRsTG5FdVZQLTZFUlpJIn0.W-ItRKPJef8DU-UsYwNQ3eZQpdsz4C7316gzM_dbPMAydYSUDe3T54uI68zfp91IdccABHBuXFVist9beXQoixUWG0FzII2mCeyX7ZgIZYjHxQ5I2oQMxH46sKaDlvmS-EtelVOqdNMwEVzstW5Fs7FolIp9Hi555IXcsD5k77yeZ2TAX6dLjm-Z53vXYdfnyoYbobcSsX6kj0fthSCh6pV5QuqxcM-p224YDmDzIhBgTihrRjP5TsgmCVJaWDkeQx5RrRq3bqXCoF8-bBjymhw5IFPpkBxsngCsVn-gYwtnSKWxo3N5o1ouhjThCGc_j0yIQnzBSM_By8eIdiTuYQ"}
+            .oidcAuthProvider { AuthenticationServiceImpl.idToken }
             .awsConfiguration( AWSConfiguration(requireContext()))
             // If you are using complex objects (S3) then uncomment
             //.s3ObjectManager(new S3ObjectManagerImplementation(new AmazonS3Client(AWSMobileClient.getInstance())))
             .build();
+
         query()
 
         return binding.root
@@ -59,19 +60,9 @@ class HomeFragment : Fragment() {
     fun query() {
         mAWSAppSyncClient.query(ListTasksQuery.builder().build())
             .responseFetcher(AppSyncResponseFetchers.CACHE_AND_NETWORK)
-            .enqueue(todosCallback)
+            .enqueue(viewModel.todosCallback)
     }
 
-    private val todosCallback: GraphQLCall.Callback<ListTasksQuery.Data> =
-        object : GraphQLCall.Callback<ListTasksQuery.Data>() {
-            override fun onResponse(response: com.apollographql.apollo.api.Response<ListTasksQuery.Data>) {
-                Log.i("Results", response.data()?.listTasks()?.items().toString())
-            }
-
-            override fun onFailure(@Nonnull e: ApolloException) {
-                Log.e("ERROR", e.toString())
-            }
-        }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/com/wwm/todo/HomeFragmentViewModel.kt
+++ app/src/main/java/com/wwm/todo/HomeFragmentViewModel.kt
@@ -1,15 +1,24 @@
 package com.wwm.todo
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.*
+import com.amazonaws.amplify.generated.graphql.ListTasksQuery
+import com.amazonaws.mobile.config.AWSConfiguration
+import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient
+import com.amazonaws.mobileconnectors.appsync.fetcher.AppSyncResponseFetchers
+import com.apollographql.apollo.GraphQLCall
+import com.apollographql.apollo.exception.ApolloException
 import com.wwm.todo.auth.AuthenticationServiceImpl
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.launch
 import timber.log.Timber
+import javax.annotation.Nonnull
 
 @ExperimentalCoroutinesApi
-class HomeFragmentViewModel : ViewModel(){
+class HomeFragmentViewModel constructor() : ViewModel(){
+
     private val handler = CoroutineExceptionHandler { _, throwable ->
         Timber.e(throwable)
     }
@@ -29,9 +38,28 @@ class HomeFragmentViewModel : ViewModel(){
         }
     }
 
+    val todosCallback: GraphQLCall.Callback<ListTasksQuery.Data> =
+        object : GraphQLCall.Callback<ListTasksQuery.Data>() {
+            override fun onResponse(response: com.apollographql.apollo.api.Response<ListTasksQuery.Data>) {
+                Log.i("Results", response.data()?.listTasks()?.items().toString())
+                val list = mutableListOf<TaskItem>()
+                response.data()?.listTasks()?.items().let {
+                    it?.forEach {
+                        val item = TaskItem(it.id(), it.title(), it.description(), it.status())
+                        list.add(item)
+                    }
+                }
+
+                _todoList.postValue(list)
+            }
+
+            override fun onFailure(@Nonnull e: ApolloException) {
+                Log.e("ERROR", e.toString())
+            }
+        }
 }
 
-class HomeFragmentViewModelFactory :
+class HomeFragmentViewModelFactory() :
     ViewModelProvider.Factory {
     @ExperimentalCoroutinesApi
     @Suppress("unchecked_cast")
